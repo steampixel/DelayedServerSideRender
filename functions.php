6072754b022e08061bebdf1ba9eb32636799284f
SteamPixel <info@steampixel.de>
2018-11-14 10:25:37 +0100

    Added example block

--- functions.php
+++ functions.php
@@ -8,3 +8,26 @@ add_action( 'init', function(){
       array( 'wp-blocks', 'wp-element' )
   );
 } );
+
+// Add example block
+add_action( 'init', function() {
+
+    wp_register_script(
+      'my_block',
+      get_template_directory_uri().'/my_block.js',
+      array( 'wp-blocks', 'wp-element', 'DelayedServerSideRender' )
+    );
+
+    register_block_type( 'my/block', [
+      'attributes' => array(
+        'test' => array(
+          'type' => 'string',
+        )
+      ),
+      'editor_script' => 'my_block',
+      'render_callback' => function($attributes, $content) {
+        return 'Server side rendered: '.print_r($attributes,true);
+      }
+    ]);
+
+});
